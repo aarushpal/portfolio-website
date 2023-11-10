db4aab50bc78e7d969c677337d57e866b1488b09
Aarush Pal <aarushpal2@gmail.com>
2023-11-11 02:53:52 +0530

    initial commit

---
+++ components/projects.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import React, { useEffect } from "react";
+import SectionHeading from "./section-heading";
+import { projectsData } from "@/lib/data";
+import Project from "./project";
+import { useActiveSectionContext } from "@/context/active-section-context";
+import { useSectionInView } from "@/lib/hooks";
+import { useInView } from "react-intersection-observer";
+
+export default function Projects() {
+  const { ref } = useSectionInView("Projects", 0.2);
+
+  return (
+    <section ref={ref} id="projects" className="scroll-mt-28 mb-28">
+      <SectionHeading>My projects</SectionHeading>
+      <div>
+        {projectsData.map((project, index) => (
+          <React.Fragment key={index}>
+            <Project {...project} />
+          </React.Fragment>
+        ))}
+      </div>
+    </section>
+  );
+}
---
+++ lib/data.ts
@@ -0,0 +1,106 @@
+import React from "react";
+// import { CgWorkAlt } from "react-icons/cg";
+// import { FaReact } from "react-icons/fa";
+import { LuGraduationCap } from "react-icons/lu";
+import gmailCloneImage from "@/public/gmailCloneImage.png";
+import OsmosisImage from "@/public/OsmosisImage.png";
+import autozoneImage from "@/public/autozoneImage.png";
+
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  {
+    name: "Experience",
+    hash: "#experience",
+  },
+
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+] as const;
+
+export const experiencesData = [
+  {
+    title: "Tax Technology Intern",
+    company: "Deloitte USI",
+    location: "Hyderabad, India",
+    description:
+      "Successfully automated testing procedures for user workflow in Sanity Testing, resulting in enhanced productivity and improved accuracy in the client application. Implemented changes resulting in reduction in error rates, faster delivery times and a 40% decrease in testing time, resulting in saving up to 8 man - hours per testing cycle; directly improving business value and enhancing customer experience",
+    icon: React.createElement(LuGraduationCap),
+    date: "June 2023 - July 2023",
+  },
+] as const;
+
+export const projectsData = [
+  {
+    title: "Gmail Clone",
+    description:
+      "Developed Gmail clone with all the core functionalities.Implemented sending emails, starring, saving drafts and deleting emails.Integrated Material UI for an intuitive user interface and deployed on Cyclic",
+    tags: ["React", "Node.js", "MongoDB", "Express", "MaterialUI"],
+    imageUrl: gmailCloneImage,
+    projectLink: "https://gleaming-jay-bracelet.cyclic.app/emails/",
+  },
+  {
+    title: "AutoZone - Car Dealership Website",
+    description:
+      "Built a Car dealership website using Django Best Practices including Django Admin customization, Django Pagination, Django messages and User Authentication.Incorporated various features like searching and filter by name, model, price or location, generate enquiry for a particular car, and enquiring via email.",
+    tags: ["Django", "PostgreSQL", "Python", "CSS"],
+    imageUrl: autozoneImage,
+    projectLink: "https://github.com/aarushpal/carzone-project",
+  },
+  {
+    title: "Osmosis Healthcare Guidance System",
+    description:
+      "Website that allows users to book and schedule doctors' appointments and for doctors to approve them.Consists of several features such as user authentication, separate user interfaces for users, doctors, and Admin.Apply for doctor and notifications for status updates for appointments",
+    tags: ["React", "Node.js", "MongoDB", "Bootstrap"],
+    imageUrl: OsmosisImage,
+    projectLink: "https://github.com/aarushpal/Osmosis-Healthcare-project",
+  },
+] as const;
+
+export const skillsData = [
+  "Data Structures And Algorithms",
+  "Operating Systems",
+  "Database Management Systems",
+  "Computer Networks",
+  "Cloud Computing",
+  "C",
+  "C++",
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "Next.js",
+  "Node.js",
+  "Git",
+  "Tailwind",
+  "SQL",
+  "MongoDB",
+  "Express",
+  "PostgreSQL",
+  "Python",
+  "Django",
+  "Flask",
+  "Framer Motion",
+  "Selenium",
+  "C#",
+  "Testing Automation",
+  "Kubernetes",
+  "Docker",
+] as const;
